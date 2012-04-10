68a78e3c83a4e45dc31404a7cac10f00a71d3894
Andrew Cutler <macropin@gmail.com>
2012-04-10 21:14:03 +1000

    Add django dep

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='bcp',
       version='0.1',
       url='https://github.com/adlibre/django-bcp',
       packages=['bcp',],
-      install_requires=['reportlab',],
+      install_requires=['django','reportlab',],
      )
