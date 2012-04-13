2fe118cee75ce35bc4b4ae4cae665a6c53759126
Andrew Cutler <macropin@gmail.com>
2012-04-13 13:21:08 +1000

    Fix deployment of templates

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@
 from distutils.core import setup
 
 setup(name='bcp',
-      version='0.1',
-      url='https://github.com/adlibre/django-bcp',
-      packages=['bcp',],
-      install_requires=['django','reportlab',],
-     )
+    version='0.1.1',
+    url='https://github.com/adlibre/django-bcp',
+    packages=['bcp',],
+    install_requires=['django','reportlab',],
+    package_data={ 'bcp': ['templates/*',] },
+)
