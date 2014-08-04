94b0113ca2cd481875b74e051873af8f9c3b4e15
Andrew Cutler <macropin@gmail.com>
2014-08-04 16:21:45 +1000

    Update for PyPi release

--- setup.py
+++ setup.py
@@ -2,14 +2,30 @@
 
 from setuptools import setup
 
+VERSION = '0.2.0'
+
 setup(name='django-bcp',
-    version='0.1.9',
+    version=VERSION,
+    author="Andrew Cutler",
+    author_email="andrew@adlibre.com.au",
+    description="Barcode Printer for Django",
+    license="BSD",
     long_description=open('README.md').read(),
     url='https://github.com/adlibre/django-bcp',
     packages=['bcp',],
+    download_url='https://github.com/adlibre/python-bureaucrat/archive/v%s.tar.gz' % VERSION,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Topic :: Utilities",
+    ],
     install_requires=[
     	'django', 
-    	'reportlab==2.6', 
+    	'reportlab>=2.6', 
     ],
     package_data={'bcp': ['fonts/*', 'templates/*', ]},
 )
